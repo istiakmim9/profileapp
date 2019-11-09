50dff753ed93c0fea76ac0d97c8421f6f8192964
istiakmim9 <istiakmim9@gmail.com>
2019-11-09 09:02:13 +0600

    profileapp-o file added

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".center").slick({
+        dots: false,
+        infinite: true,
+        centerMode: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+
+    });
+
+
+    var prev = $(".slick-prev.slick-arrow").addClass('shadow');
+    prev.html("<i class='fas fa-chevron-left'></i>");
+
+    var next = $(".slick-next.slick-arrow").addClass('shadow');
+    next.html("<i class='fas fa-chevron-right'></i>");
+
+
+
+});
+
