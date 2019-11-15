f3bf1a09a238cbcb2215ce1d9696bec28c941dac
istiakmim9 <istiakmim9@gmail.com>
2019-11-15 22:28:57 +0600

    end webpage.html redesign

--- js/custom.js
+++ js/custom.js
@@ -22,10 +22,13 @@ $(document).ready(function(){
 	$('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);}, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);});
+	*/
+    
+    // $('#publicpage').on('click', function () {
+    //     $('#createpagemodal').modal('hide');
+    //     $('#publicmodal').modal('show');
+    // });
 
-*/	
-	
-	
 	
 
 
